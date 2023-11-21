a4c0c19c0248b2c8c25d55fc5154bda7be4c4be9
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-11-21 13:50:45 +0100

    how add SVG images or icons to project

--- images,asset,public/pages/index.vue
+++ images,asset,public/pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>Main Page</div>
-  <img src="~/assets/audia3-1.webp" alt="" /> // this is first way
+  <!-- <img src="~/assets/audia3-1.webp" alt="" /> // this is first way -->
 
   <!-- <img src="@/assets/audia3-1.webp" alt="" /> //This is second way -->
+
+  <IconsBell />
 </template>
 
 <script setup></script>
