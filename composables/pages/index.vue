da6b6dd9f01e853e5a1bb7bdf4e9a587faad4fc4
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-11-21 14:17:51 +0100

    added composables and get useUtils in our index.vue page

---
+++ composables/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Main Page</div>
+</template>
+
+<script setup>
+const { sayHello } = useUtils(); //we can get useUtils from composables here
+sayHello();
+</script>
+
+<style lang="scss" scoped></style>
---
+++ composables/pages/profiles/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>This is Profile page</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
