5d8ab70d3959e7e9abb9545af3b62959be6c0df2
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-11-21 23:10:19 +0100

    used useState management

---
+++ state_management/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const counter = useCounter();
+</script>
+
+<template>
+  <div>
+    <div>
+      Counter: {{ counter }}
+      <button @click="counter++">+</button>
+      <button @click="counter--">-</button>
+    </div>
+    <Counter />
+  </div>
+</template>
