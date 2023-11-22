8ad5d68b8e3319df5faeac582d30afbbff748368
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-11-22 08:03:33 +0100

    added Hybrid Rendering to nuxt.config.ts

--- server/nuxt.config.ts
+++ server/nuxt.config.ts
@@ -6,14 +6,16 @@ export default defineNuxtConfig({
     '@': resolve(__dirname, './src'), // This is first way
     // assets: '/<rootDir>/assets', // This is second way
   },
-  modules: [
-    '@pinia/nuxt',
-  ],
+  modules: ['@pinia/nuxt'],
   css: ['~/assets/main.scss'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  routeRules: {
+    // Homepage pre-rendered at build time
+    '/': { prerender: true },
   },
 });
