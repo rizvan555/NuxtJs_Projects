234ed88500c0d4cf0bae92367663290b06e56102
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-11-22 10:54:06 +0100

    added Pending and LazyFetching to project

--- fetching_async/pages/index.vue
+++ fetching_async/pages/index.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-import { transform } from '@vue/compiler-core';
+// const { data: products } = await useFetch('/api/products', {
+//   transform: (_products) => _products.data, //for delete "data" key from response
+// });
 
-const { data: products } = await useFetch('/api/products', {
-  transform: (_products) => _products.data, //for delete "data" key from response
+const { pending, data: products } = await useLazyFetch('/api/products', {
+  transform: (_products: any) => _products.data, //for delete "data" key from response
 });
-
-console.log(toRaw(products.value));
+console.log(products);
 </script>
 
 <template>
-  <div>{{ products }}</div>
+  <div v-if="pending">Loading.....</div>
+  <div v-else>
+    <div v-for="product in products">
+      {{ product.name }}
+    </div>
+  </div>
 </template>
--- fetching_async/server/api/products.ts
+++ fetching_async/server/api/products.ts
@@ -1,7 +1,17 @@
 import data from './products.json';
 
-export default defineEventHandler(() => {
-  return {
-    data,
-  };
+// export default defineEventHandler(() => {
+//   return {
+//     data,
+//   };
+// });
+
+export default defineEventHandler(async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data,
+      });
+    }, 2000);
+  });
 });
